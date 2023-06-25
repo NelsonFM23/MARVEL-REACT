a6374da8d7a6ebd77cc6cc8133c929554a8764e9
NelsonFM23 <133304859+NelsonFM23@users.noreply.github.com>
2023-06-25 23:31:39 +0200

    first commit

---
+++ src/components/Container/Container.js
@@ -0,0 +1,12 @@
+import { Container as Content } from "semantic-ui-react";
+
+import "./Container.scss";
+
+export default function Container({children, bg}) {
+   return(
+        <div className={bg ? '': 'container-bg'}>;
+            <Content>{children}</Content>
+        </div>
+    );
+    
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import BestCharacters from "../components/BestCharacters/BestCharacters";
+import Insider from "../components/Insider/Insider";
+import ListLastEvents from "../components/ListLastEvent/ListLastEvent";
+export default function Home() {
+    
+    return (
+        
+        <div>
+            < BestCharacters/>
+            < ListLastEvents/>
+            < Insider />
+        
+        </div>
+        
+    );
+}
